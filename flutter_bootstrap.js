d58c17d6393cc8a9742850eb563be3d32393ab67
Sheik Ahamed <sdsheikahamed@gmail.com>
2025-03-16 20:54:46 +0530

    Added some features and fixed responsiveness

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3265595080"
+    serviceWorkerVersion: "1388768261"
   }
 });
