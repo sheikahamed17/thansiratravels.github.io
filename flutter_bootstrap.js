36922cb91607dba7721bcd4abb0f37eec3c4191b
Sheik Ahamed <57103479+sheikahamed17@users.noreply.github.com>
2025-04-04 21:10:58 +0530

    Update flutter_bootstrap.js

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -10,7 +10,7 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 
 _flutter.loader.load({
-  serviceWorkerSettings: {
-    serviceWorkerVersion: "3982860068"
-  }
+  //serviceWorkerSettings: {
+    //serviceWorkerVersion: "3982860068"
+  //}
 });
